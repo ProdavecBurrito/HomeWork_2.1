dd13cb436ff3cc2648d6345fb881a324def616e5
Михаил <shipov-mihail@mail.ru>
2020-01-21 22:06:37 +0300

    WIP: Fixed file storage

--- Planet.cs
+++ Planet.cs
@@ -12,14 +12,6 @@ namespace HomeWork_2_1
             color = _color;
             image = _image;
         }
-        public override void Update()
-        {
-            Pos.X = Pos.X + Dir.X;
-            if (Pos.X < 0)
-            {
-                Pos.X = Game.Width + Size.Width;
-            }
-        }
 
         public override void Draw()
         {
--- Star.cs
+++ Star.cs
@@ -15,13 +15,5 @@ namespace HomeWork_2_1
             Game.Buffer.Graphics.DrawLine(Pens.White, Pos.X + Size.Width, Pos.Y, Pos.X, Pos.Y + Size.Height);
         }
 
-        public override void Update()
-        {
-            Pos.X = Pos.X + Dir.X;
-            if (Pos.X < 0)
-            {
-                Pos.X = Game.Width + Size.Width;
-            }
-        }
     }
 }
